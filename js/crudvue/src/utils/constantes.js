6c1c5cd9ed0278c5892e5133cd1e9b23e9bc57d1
Twovski <L20170764@culiacan.tecnm.mx>
2023-11-22 11:02:11 -0700

    Inicio de Vue.js

---
+++ js/crudvue/src/utils/constantes.js
@@ -0,0 +1,1 @@
+export const URL_DATOS="http://localhost:300/api";
---
+++ js/crudvue/src/views/Alumnos.vue
@@ -0,0 +1,19 @@
+<template>
+    <AlumnosLista/>
+</template>
+<script>
+    import AlumnosLista from "../components/AlumnosLista.vue"
+    export default {
+        name: "Alumnos",
+        components: { AlumnosLista },
+        data: () => {
+            return{};
+        },
+        mounted: () => {
+
+        },
+        methods: () => {
+
+        },
+    }
+</script>
